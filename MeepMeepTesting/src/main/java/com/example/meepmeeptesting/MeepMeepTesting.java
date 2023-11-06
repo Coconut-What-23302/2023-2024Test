a7ce8837df558079782c6b3747f003d6561891dd
Rhys <rhyspanopio22@gmail.com>
2023-11-06 00:44:34 -0800

    back red path test

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -16,9 +16,38 @@ public class MeepMeepTesting {
                 .setConstraints(52.48180821614297, 52.48180821614297,  2.793466, Math.toRadians(184.02607784577722), 13.7)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-39, -70, Math.toRadians(90)))
-                                .splineTo(new Vector2d(-36, -23), 0)
-                                .strafeLeft(10)
-                                .forward(45)
+
+                                // don't touch
+
+//                                .splineToConstantHeading(new Vector2d(-55.73, -34.66), Math.toRadians(115.52))
+
+                                .splineToLinearHeading(new Pose2d(-57.61, -36.98, Math.toRadians(70.94)), Math.toRadians(70.94))
+                                .splineToLinearHeading(new Pose2d(-36.00, -23.00, Math.toRadians(0.00)), Math.toRadians(0.00))
+                                .addDisplacementMarker(() -> {
+                                    // grab wobble goal
+                                })
+                                .lineTo(new Vector2d(-40.61, -14.75))
+                                .splineToConstantHeading(new Vector2d(-28.55, -9.52), Math.toRadians(8.87))
+                                .splineToConstantHeading(new Vector2d(-16.06, -13.15), Math.toRadians(-9.82))
+                                .splineToConstantHeading(new Vector2d(-4.00, -10.53), Math.toRadians(15.87))
+                                .splineToConstantHeading(new Vector2d(7.63, -9.52), Math.toRadians(10.45))
+                                .splineToConstantHeading(new Vector2d(17.22, -16.20), Math.toRadians(7.22))
+                                .splineToConstantHeading(new Vector2d(28.99, -10.97), Math.toRadians(30.68))
+                                .splineToConstantHeading(new Vector2d(33.35, -8.06), Math.toRadians(42.85))
+                                .splineToConstantHeading(new Vector2d(27.39, -65.17), Math.toRadians(252.09))
+                                .lineToSplineHeading(new Pose2d(47.73, -35.24, Math.toRadians(180.00)))
+
+
+
+
+
+
+
+
+
+
+
+                                // do not touch
                                 .build()
 
                 );
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/myOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/myOpmode.java
@@ -34,25 +34,29 @@ public class myOpmode extends LinearOpMode {
         drive.setPoseEstimate(startPose);
 
         Trajectory untitled0 = drive.trajectoryBuilder(startPose)
-                .splineTo(new Vector2d(-36.79, -23.00), Math.toRadians(0.00))
-                .splineTo(new Vector2d(-46.66, -9.41), Math.toRadians(103.71))
-                .splineTo(new Vector2d(-40.52, -6.24), Math.toRadians(0.00))
-                .splineTo(new Vector2d(37.72, -21.14), Math.toRadians(-6.37))
+                //
+                .splineToLinearHeading(new Pose2d(-57.61, -36.98, Math.toRadians(70.94)), Math.toRadians(70.94))
+                .splineToLinearHeading(new Pose2d(-36.00, -23.00, Math.toRadians(0.00)), Math.toRadians(0.00))
+                .addDisplacementMarker(() -> {
+                    // drop the servo which i totally did not break
+                })
+                .lineTo(new Vector2d(-40.61, -14.75))
+                .splineToConstantHeading(new Vector2d(-28.55, -9.52), Math.toRadians(8.87))
+                .splineToConstantHeading(new Vector2d(-16.06, -13.15), Math.toRadians(-9.82))
+                .splineToConstantHeading(new Vector2d(-4.00, -10.53), Math.toRadians(15.87))
+                .splineToConstantHeading(new Vector2d(7.63, -9.52), Math.toRadians(10.45))
+                .splineToConstantHeading(new Vector2d(17.22, -16.20), Math.toRadians(7.22))
+                .splineToConstantHeading(new Vector2d(28.99, -10.97), Math.toRadians(30.68))
+                .splineToConstantHeading(new Vector2d(33.35, -8.06), Math.toRadians(42.85))
+                .splineToConstantHeading(new Vector2d(27.39, -65.17), Math.toRadians(252.09))
+                .lineToSplineHeading(new Pose2d(47.73, -35.24, Math.toRadians(180.00)))
+                //
                 .build();
 
 
-        Trajectory untitled1 = drive.trajectoryBuilder(untitled0.end())
-                .splineTo(new Vector2d(-46.66, -9.41), Math.toRadians(103.71))
-                .splineTo(new Vector2d(-40.52, -6.24), Math.toRadians(0.00))
-                .splineTo(new Vector2d(37.72, -21.14), Math.toRadians(-6.37))
-                .build();
 
         drive.followTrajectory(untitled0);
-        robot.spikeMarkDrop.setPosition(-1);
-     sleep(200);
-        robot.spikeMarkDrop.setPosition(1);
-        sleep(600);
-        drive.followTrajectory(untitled1);
+
 
 
     }
